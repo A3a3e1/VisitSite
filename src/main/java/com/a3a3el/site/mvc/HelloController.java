9c9cb123a4566d251b54c102ef248f962eda8b60
A3a3e1 <voropaiev@gmail.com>
2015-11-24 14:49:03 +0200

    Added template for site

--- src/main/java/com/a3a3el/site/mvc/HelloController.java
+++ src/main/java/com/a3a3el/site/mvc/HelloController.java
@@ -3,6 +3,7 @@ package com.a3a3el.site.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.EntityManager;
@@ -12,6 +13,7 @@ import javax.persistence.Persistence;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
 	public HelloController() {
 	}
 
